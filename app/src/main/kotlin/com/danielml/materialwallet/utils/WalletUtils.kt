b3ebf129d66af8040ca13d54041f3187e3cba4bc
daniml3 <daniel@danielml.dev>
2021-12-25 22:30:29 +0100

    app: implement a transaction list
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: Ica88c22ad2238bb6bcc1bbf77631ddc5bc9aaf8a

---
+++ app/src/main/kotlin/com/danielml/materialwallet/utils/WalletUtils.kt
@@ -0,0 +1,24 @@
+package com.danielml.materialwallet.utils
+
+import com.danielml.materialwallet.Global
+import org.bitcoinj.core.Transaction
+import org.bitcoinj.core.TransactionOutput
+import org.bitcoinj.kits.WalletAppKit
+
+class WalletUtils {
+    companion object {
+        fun calculateTransactionValue(walletKit: WalletAppKit, transaction: Transaction, isIncoming: Boolean): Long {
+            var value: Long = 0
+
+            for (output: TransactionOutput in transaction.outputs) {
+                val address = output.scriptPubKey.getToAddress(Global.NETWORK_PARAMS)
+                val addressMine = walletKit.wallet().isAddressMine(address)
+                if ((addressMine && isIncoming) || (!addressMine && !isIncoming)) {
+                    value += output.value.toSat()
+                }
+            }
+
+            return value
+        }
+    }
+}
