e389585cee5b3d43a3a7f2a7948108a8a559fe79
daniml3 <daniel@danielml.dev>
2021-12-26 20:27:38 +0100

    app: switch to btc as display unit
    Change-Id: I08b74976e83b7465ff9c3867a877def22187447e

--- app/src/main/kotlin/com/danielml/materialwallet/utils/WalletUtils.kt
+++ app/src/main/kotlin/com/danielml/materialwallet/utils/WalletUtils.kt
@@ -4,17 +4,18 @@ import com.danielml.materialwallet.Global
 import org.bitcoinj.core.Transaction
 import org.bitcoinj.core.TransactionOutput
 import org.bitcoinj.kits.WalletAppKit
+import java.math.BigDecimal
 
 class WalletUtils {
     companion object {
-        fun calculateTransactionValue(walletKit: WalletAppKit, transaction: Transaction, isIncoming: Boolean): Long {
-            var value: Long = 0
+        fun calculateTransactionValue(walletKit: WalletAppKit, transaction: Transaction, isIncoming: Boolean): BigDecimal {
+            var value = BigDecimal(0)
 
             for (output: TransactionOutput in transaction.outputs) {
                 val address = output.scriptPubKey.getToAddress(Global.NETWORK_PARAMS)
                 val addressMine = walletKit.wallet().isAddressMine(address)
                 if ((addressMine && isIncoming) || (!addressMine && !isIncoming)) {
-                    value += output.value.toSat()
+                    value += output.value.toBtc()
                 }
             }
 
